7ea3bd183e37724562e4def9d7148f431973347e
Manickavaasan T S <vasuvaasan5656@gmail.com>
2025-07-04 23:00:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+var randomNO1=Math.floor(Math.random()*6)+1;
+var dice1="images/"+"dice"+randomNO1+".png";
+var img1=document.querySelectorAll("img")[0];
+img1.setAttribute("src",dice1);
+
+var randomNo2=Math.floor(Math.random()*6)+1;
+var dice2="images/"+"dice"+randomNo2+".png";
+var img2=document.querySelectorAll("img")[1];
+img2.setAttribute("src",dice2);
